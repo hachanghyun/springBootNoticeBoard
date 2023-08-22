0f02b19e942fc714e4cdf58b9011b7b6ed5413af
hachanghyun <haccwx@naver.com>
2023-08-22 21:23:14 +0900

    커밋메시지

---
+++ src/main/java/com/example/springBootNoticeBoard/SpringBootNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootNoticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNoticeBoardApplication.class, args);
+	}
+
+}
