25122ebf45d69436bac0ce62c580b21daddd0c78
hachanghyun <haccwx@naver.com>
2023-08-24 22:08:35 +0900

    예제 commit

--- src/test/java/com/example/springBootNoticeBoard/web/HelloControllerTest.java
+++ src/test/java/com/example/springBootNoticeBoard/web/HelloControllerTest.java
@@ -8,10 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
 class HelloControllerTest {
@@ -28,4 +27,17 @@ class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
 }
