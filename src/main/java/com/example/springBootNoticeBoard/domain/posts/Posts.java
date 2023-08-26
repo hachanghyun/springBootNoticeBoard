a1c64d5afcb4a7cbbb9d2486a38efb4a208c9d40
hachanghyun <haccwx@naver.com>
2023-08-26 22:53:17 +0900

    예제 commit

--- src/main/java/com/example/springBootNoticeBoard/domain/posts/Posts.java
+++ src/main/java/com/example/springBootNoticeBoard/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.example.springBootNoticeBoard.domain.posts;
 
+import com.example.springBootNoticeBoard.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Generated;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,5 +31,11 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+
     }
 }
--- src/main/java/com/example/springBootNoticeBoard/service/posts/PostsService.java
+++ src/main/java/com/example/springBootNoticeBoard/service/posts/PostsService.java
@@ -1,11 +1,19 @@
 package com.example.springBootNoticeBoard.service.posts;
 
+import com.example.springBootNoticeBoard.domain.posts.Posts;
 import com.example.springBootNoticeBoard.domain.posts.PostsRepository;
+import com.example.springBootNoticeBoard.web.dto.PostsListResponseDto;
+import com.example.springBootNoticeBoard.web.dto.PostsResponseDto;
 import com.example.springBootNoticeBoard.web.dto.PostsSaveRequestDto;
+import com.example.springBootNoticeBoard.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.*;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,5 +24,34 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
